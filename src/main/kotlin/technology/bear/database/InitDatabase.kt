41f821b57ac1ac102b3169746022ddb6b996f31c
Daniil Titov <Daniil.A.Titov@gmail.com>
2020-01-20 00:14:31 +0300

    added database schema

--- src/main/kotlin/technology/bear/database/InitDatabase.kt
+++ src/main/kotlin/technology/bear/database/InitDatabase.kt
@@ -3,7 +3,6 @@ package technology.bear.database
 import org.jetbrains.exposed.sql.Database
 import org.jetbrains.exposed.sql.SchemaUtils
 import org.jetbrains.exposed.sql.transactions.transaction
-import technology.bear.database.dao.Cities
 import technology.bear.database.dao.Tasks
 
 fun initDatabase() {
